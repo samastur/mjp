d1b1ddec5f6f4841035e2be9ef2694c5389c2c3d
Marko Samastur <markos@gaivo.net>
2013-10-17 10:53:25 +0200

    Added events module
    Changes:
    - added method .on, .off and .trigger
    - add currently very basic normalizer for events for differences
      between browsers
    - test for current functionality

--- src/events.js
+++ src/events.js
@@ -1,17 +1,61 @@
 define([
     "./core"
 ], function (mjp) {
+    mjp.handlers = {};
+
+    function normalizeEvent(e) {
+        return e;
+    }
+
+    function getHandlerId() {
+        var id;
+        while((id=Math.ceil(Math.random()*10000)) in mjp.handlers) {}
+        return id;
+    }
+
+    function wrapHandler(func) {
+        var f = function (e) {
+            var ev = e || normalizeEvent(window.event);
+            return func.call(this, ev);
+        };
+        if (!func.mjp) {
+            func.mjp = getHandlerId();
+            mjp.handlers[func.mjp] = f;
+        }
+        return mjp.handlers[func.mjp];
+    }
+
+    function getHandler(func) {
+        return func.mjp ? mjp.handlers[func.mjp] : func;
+    }
+
     // Event handlers
     mjp.extend(mjp.fn, {
-        on: function(ev_type, handler) { // For IE8
+        on: function(ev_type, handler) {
             this.each(function (i, node) {
                 if (node.addEventListener) {
-                    node.addEventListener(ev_type, handler);
-                } else if (node.attachEvent) {
-                    node.attachEvent("on"+ev_type, handler);
+                    node.addEventListener(ev_type, wrapHandler(handler));
+                } else if (node.attachEvent) {  // For IE8
+                    node.attachEvent("on"+ev_type, wrapHandler(handler));
                 }
             });
             return this;
+        },
+
+        off: function (ev_type, handler) {
+            this.each(function (i, node) {
+                if (node.removeEventListener) {
+                    node.removeEventListener(ev_type, getHandler(handler));
+                } else if (node.detachEvent) { // For IE8
+                    node.detachEvent("on"+ev_type, getHandler(handler));
+                }
+            });
+        },
+
+        trigger: function (ev_type) {
+            this.each(function (i, node) {
+                node[ev_type] && node[ev_type]();
+            });
         }
     });
 
