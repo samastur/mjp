4b703149ea16a6901c2cee41384178e8ee159f70
Marko Samastur <markos@gaivo.net>
2013-10-21 00:30:15 +0200

    Added event normalizer

--- src/events.js
+++ src/events.js
@@ -4,6 +4,23 @@ define([
     mjp.handlers = {};
 
     function normalizeEvent(e) {
+        var ev = {originalEvent: e,
+                  timeStamp: (new Date()).getTime()},
+            copy = ["type", "clientX", "clientY", "altKey", "ctrlKey", "shiftKey"];
+
+        mjp(e).each(function (i, attr) { ev[attr] = e[attr]; });
+        mjp(copy).each(function (i, attr) { ev[attr] = e[attr]; });
+
+        ev.currentTarget = this;
+        ev.target = e.target || e.srcElement;
+        ev.charCode = e.charCode || e.keyCode;
+        ev.eventPhase = this === ev.target ? 2 : 3;
+        ev.relatedTarget = e.relatedTarget || e.fromElement || e.toElement;
+
+        if (!e.preventDefault) {
+            e.stopPropagation = function () { this.cancelBubble = true; };
+            e.preventDefault = function () { this.returnValue = false; };
+        }
         return e;
     }
 
@@ -15,7 +32,7 @@ define([
 
     function wrapHandler(func) {
         var f = function (e) {
-            var ev = e || normalizeEvent(window.event);
+            var ev = normalizeEvent.call(this, e || window.event);
             return func.call(this, ev);
         };
         if (!func.mjp) {
@@ -35,7 +52,7 @@ define([
             this.each(function (i, node) {
                 if (node.addEventListener) {
                     node.addEventListener(ev_type, wrapHandler(handler));
-                } else if (node.attachEvent) {  // For IE8
+                } else {  // For IE8
                     node.attachEvent("on"+ev_type, wrapHandler(handler));
                 }
             });
@@ -46,7 +63,7 @@ define([
             this.each(function (i, node) {
                 if (node.removeEventListener) {
                     node.removeEventListener(ev_type, getHandler(handler));
-                } else if (node.detachEvent) { // For IE8
+                } else { // For IE8
                     node.detachEvent("on"+ev_type, getHandler(handler));
                 }
             });
