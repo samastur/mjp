550e7a39a8d0084e965f986176cacb3360cd5d0b
Marko Samastur <markos@gaivo.net>
2013-11-16 17:02:04 +0100

    Bump version and fix configuration bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,7 @@ module.exports = function( grunt ) {
             }
         },
         watch: {
-            files: [ "src/**/*.js", "test/**/*.html" ],
+            files: [ "src/**/*.js", "tests/**/*.html" ],
             tasks: ["test"]
         },
         "pre-uglify": {
